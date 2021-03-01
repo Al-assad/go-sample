e6df69ba13ecf66c2bb29eea40ab034625aa8a18
yulinying <yulinying_1994@outlook.com>
2021-03-01 22:30:24 +0800

    update

---
+++ src/spl.assad/main/f1_oop_struct_method_spl2.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// go struct 结构体示例，字段 Getter、Setter
+
+// 简单结构体
+type User3 struct {
+	name  string
+	scope int
+}
+
+func (u *User3) GetName() string {
+	return u.name
+}
+
+func (u *User3) GetScope() int {
+	return u.scope
+}
+
+func (u *User3) SetName(name string) {
+	u.name = name
+}
+
+func (u *User3) setScope(scope int) {
+	u.scope = scope
+}
+
+func main() {
+	u := &User3{"Assad", 1024}
+	fmt.Println(u.GetName(), u.GetScope())
+	u.SetName("Alex")
+	u.setScope(2048)
+	fmt.Println(u.GetName(), u.GetScope())
+	//Assad 1024
+	//Alex 2048
+}
