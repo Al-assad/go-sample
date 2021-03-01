ecaa815e6540214cc8d627ffe7ff1dda1eec2783
yulinying <yulinying_1994@outlook.com>
2021-03-01 14:36:41 +0800

    update

--- src/spl.assad/main/e1_thirdparty_vandor_spl.go
+++ src/spl.assad/main/e1_thirdparty_vandor_spl.go
@@ -13,10 +13,14 @@ import (
 // 方式 1：go get
 //        GOPATH=${project_path} && cd ${project_path}/src
 //        go get github.com/deckarep/golang-set
+
 // 方式 2：vendor
 //        GOPATH=${project_path} && cd ${project_path}/src
 //		  govendor init
 //        govendor fetch github.com/deckarep/golang-set
+
+//        已存在 ./vendor 目录，根据 .vendor/vendor.json 同步所有第三方库元源码
+//        govendor sync
 
 func main() {
 	mlist := []int{1, 2, 3, 4, 5, 6, 7, 7, 2, 1, 2}
