0b05c7b96ef0bd472ed3cbed538693b2106e55f5
yulinying <yulinying_1994@outlook.com>
2021-03-02 18:16:13 +0800

    update

--- src/spl.assad/main/d8_standpkg_encode_decode_spl.go
+++ src/spl.assad/main/d8_standpkg_encode_decode_spl.go
@@ -1,7 +1,41 @@
 package main
 
-// go 加解密处理
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"fmt"
+	"io"
+)
+
+// go 加解密处理（sha、md5 等）
+// 标准库加解-编解码包位于 encoding 包下：https://golang.org/pkg/encoding/
+
+// hash 包：实现了 adler32、crc32、crc64 和 fnv 校验；
+// crypto 包：实现了其它的 hash 算法，比如 md4、md5、sha1 等。
+//			  以及完整地实现了 aes、blowfish、rc4、rsa、xtea 等加密算法。
 
 func main() {
+
+	str1 := "Whenever you need me, I'll be here."
+	str2 := "A sonnet in every tree, a tale in every lifetime, its just for you to see…"
+
+	// 计算 Hash SHA1 校验值
+	shaHash := sha1.New()
+	b := []byte{}
+
+	io.WriteString(shaHash, str1)
+	checksum1 := shaHash.Sum(b)   // 计算 str1 sha1 校验和
+	fmt.Printf("%X\n", checksum1) // D57ACBB1116583070B11D8206FC5FC7B25FC6E52
+
+	shaHash.Reset()
+	io.WriteString(shaHash, str2)
+	checksum2 := shaHash.Sum(b)   // 计算 str2 sha1 校验和
+	fmt.Printf("%X\n", checksum2) // 372547E7132D000BE9C515C1FECDF8FD2C56B15E
+
+	// 计算 Hash MD5 校验值
+	md5Hash := md5.New()
+	io.WriteString(md5Hash, str1)
+	checksum3 := md5Hash.Sum(b)   // 计算 str1 md5 校验和
+	fmt.Printf("%X\n", checksum3) // 6B5203F0EC116B97B2A9F5811DC26807
 
 }
--- src/spl.assad/main/f12_oop_gob_spl.go
+++ src/spl.assad/main/f12_oop_gob_spl.go
@@ -1,7 +1,43 @@
 package main
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
 // go gob 二进制序列化、反序列化
+// Gob 是 Go 自己的以二进制形式序列化和反序列化程序数据的格式, G
+// Gob 特定地用于纯 Go 的环境中，例如，两个用 Go 写的服务之间的通信。这样的话服务可以被实现得更加高效和优化
+
+// 发送方结构体
+type T struct {
+	X int
+	Y int
+	Z int
+}
+
+// 获取方结构体
+type U struct {
+	X int
+	Y int
+}
 
 func main() {
+	var network bytes.Buffer
+	// gob 编码器
+	enc := gob.NewEncoder(&network)
+	// gob 解码器
+	dec := gob.NewDecoder(&network)
+
+	// 序列化编码
+	t := &T{1, 2, 3}
+	enc.Encode(t)
+	// 反序列化解码
+	var u U
+	dec.Decode(&u)
+
+	fmt.Println("T", *t) // T {1 2 3}
+	fmt.Println("U", u)  // U {1 2}
 
 }
