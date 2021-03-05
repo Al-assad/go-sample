f9a55ca638cd0d7d9e8f57b4c1389edcbd5a1d6a
yulinying <yulinying_1994@outlook.com>
2021-03-05 17:12:53 +0800

    update

--- src/concurrent.spl.assad/main/s12_gortn_lazy_evaluation_spl.go
+++ src/concurrent.spl.assad/main/s12_gortn_lazy_evaluation_spl.go
@@ -1,17 +1,43 @@
 package main
 
+import "fmt"
+
 // @desc 实现惰性生成器
 
 func main() {
+	sequence := NewEvenSequence()
+	fmt.Println(sequence.Next()) //0
+	fmt.Println(sequence.Next()) //2
+	fmt.Println(sequence.Next()) //4
 
 }
 
-// 斐波那契数列计算
-func fibonacci2(n int) (res int) {
-	if n <= 1 {
-		res = 1
-	} else {
-		res = fibonacci(n-1) + fibonacci(n-2)
-	}
-	return res
+// 无限获取的偶数数列
+type EvenSequence struct {
+	resume chan int // 当前偶数值
+}
+
+// 构建函数
+func NewEvenSequence() *EvenSequence {
+	ch := genEven()
+	sequence := &EvenSequence{ch}
+	return sequence
+}
+
+// 获取下一个偶数
+func (e *EvenSequence) Next() int {
+	return <-e.resume
+}
+
+// 构建偶数协程，通过 channel 实现类似 yield 机制
+func genEven() chan int {
+	yield := make(chan int)
+	curVal := 0
+	go func() {
+		for {
+			yield <- curVal
+			curVal += 2
+		}
+	}()
+	return yield
 }
